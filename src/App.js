7c450b6845df1abc6d6c2fd4eb39e6cf80dfc2c4
Tofik Mahdi <aquat3@gmail.com>
2019-07-24 19:20:15 -0400

    more updates

--- src/App.js
+++ src/App.js
@@ -57,102 +57,82 @@ class Hero extends React.Component {
   }
    quote(){
   var quoteSource=[
-		{
-			quote: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.",
-			name:"Francis of Assisi"
-	    },
-	    {
-	    	quote:"Believe you can and you're halfway there.",
-	    	name:"Theodore Roosevelt"
-	    },
-	    {
-	    	quote:"It does not matter how slowly you go as long as you do not stop.",
-	    	name:"Confucius"
-	    },
-	    {
-	    	quote:"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
-	    	name:"Thomas A. Edison"
-	    },
-	    {
-	    	quote:"The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence.",
-	    	name:"Confucius"
-	    },
-	    {
-	    	quote:"Don't watch the clock; do what it does. Keep going.",
-	    	name:"Sam Levenson"
-	    },
-	    {
-	    	quote:"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
-	    	name:"Ayn Rand"
-	    },
-	    {
-	    	quote:"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
-	    	name:"Ayn Rand"
-	    },
-	    {
-	    	quote:"Expect problems and eat them for breakfast.",
-	    	name:"Alfred A. Montapert"
-	    },
-	    {
-	    	quote:"Start where you are. Use what you have. Do what you can.",
-	    	name:"Arthur Ashe"
-	    },
-	    {
-	    	quote:"Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.",
-	    	name:"Samuel Beckett"
-	    },
-	    {
-	    	quote:"Be yourself; everyone else is already taken.",
-	    	name:"Oscar Wilde"
-	    },
-	    {
-	    	quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
-	    	name:"Albert Einstein"
-	    },
-	    {
-	    	quote:"Always remember that you are absolutely unique. Just like everyone else.",
-	    	name:"Margaret Mead"
-	    },
-	    {
-	    	quote:"Do not take life too seriously. You will never get out of it alive.",
-	    	name:"Elbert Hubbard"
-	    },
-	    {
-	    	quote:"People who think they know everything are a great annoyance to those of us who do.",
-	    	name:"Isaac Asimov"
-	    },
-	    {
-	    	quote:"Procrastination is the art of keeping up with yesterday.",
-	    	name:"Don Marquis"
-	    },
-	    {
-	    	quote:"Get your facts first, then you can distort them as you please.",
-	    	name:"Mark Twain"
-	    },
-	    {
-	    	quote:"A day without sunshine is like, you know, night.",
-	    	name:"Steve Martin"
-	    },
-	    {
-	    	quote:"My grandmother started walking five miles a day when she was sixty. She's ninety-seven now, and we don't know where the hell she is.",
-	    	name:"Ellen DeGeneres"
-	    },
-	    {
-	    	quote:"Don't sweat the petty things and don't pet the sweaty things.",
-	    	name:"George Carlin"
-	    },
-	    {
-	    	quote:"Always do whatever's next.",
-	    	name:"George Carlin"
-	    },
+		
+		 "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible."
+			 
+	    ,
+	    
+	    	"Believe you can and you're halfway there.",
+	    	 
+	    
+	     
+	    	"It does not matter how slowly you go as long as you do not stop.",
+	    	 
+	     
+	    
+	    	"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+	    	 
+	    
+	    
+	    	"The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence."
+	    	 
+	    ,
+	    
+	    	"Don't watch the clock; do what it does. Keep going.",
+	    	 
 	   
-	    {
-	    	quote:"Hapiness is not something ready made. It comes from your own actions.",
-	    	name:"Dalai Lama"
-	    }
+	    
+	    	"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
+	    	 
+	    
+	    
+	    	"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
+	    	 
+	    
+	    
+	    	"Expect problems and eat them for breakfast.",
+	    	 
+	    
+	    
+	    	"Start where you are. Use what you have. Do what you can.",
+	    	 
+	    
+	    
+	    	"Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.",
+	     
+	    
+	    
+	    	"Be yourself; everyone else is already taken.",
+	    	 
+	    
+	    
+	    	"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+	    	 
+	    
+	    "Always remember that you are absolutely unique. Just like everyone else.",
+	    	 
+	   "Do not take life too seriously. You will never get out of it alive.",
+	    	 
+	  "People who think they know everything are a great annoyance to those of us who do.",
+	    	 
+	   "Procrastination is the art of keeping up with yesterday.",
+	    	 
+	   "Get your facts first, then you can distort them as you please.",
+	    	 
+	   "A day without sunshine is like, you know, night.",
+	     
+	   "My grandmother started walking five miles a day when she was sixty. She's ninety-seven now, and we don't know where the hell she is.",
+	    	 
+	   "Don't sweat the petty things and don't pet the sweaty things.",
+	    	 
+	   "Always do whatever's next.",
+	     
+	   "Hapiness is not something ready made. It comes from your own actions.",
+	    	 
+	   
 
   ];
-  return quoteSource[Math.floor(Math.random() * 10)].quote;
+  return quoteSource[Math.floor(Math.random() * 10)];
 }
   
   render() {
@@ -184,7 +164,10 @@ class Home extends React.Component{
   return(
     <div className='aboutLeft'>
    
-    <img  src='https://images.unsplash.com/photo-1539418561314-565804e349c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'/>
+    <img  //src='https://images.unsplash.com/photo-1539418561314-565804e349c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+    src='https://i.pinimg.com/originals/84/00/1b/84001be065400e901334218eee95912e.gif'
+    alt='University of Maryland Mascot'
+    />
      
   </div>
   );
@@ -202,15 +185,15 @@ class Home extends React.Component{
         
       <ScrollAnimation animateIn='slideInRight' duration={0.3}>
         <p>Hello World, my name is Tofik Mahdi. I am a senior at the Univeristy of 
-          Maryland graduating in Fall 2019.I love the internet and I plan on 
+          Maryland graduating in Fall 2019. I love the internet and I plan on 
           leaving a digital footprint in it.</p></ScrollAnimation >
         
-        <ScrollAnimation animateIn='slideInLeft' duration={0.3}><p>I started coding since I was 17 in C++ but I eventually picked up web development 
+        <ScrollAnimation animateIn='slideInRight' duration={0.3}><p>I started coding since I was 17 in C++, but I eventually picked up web development 
           a couple years later after being curious as to how websites work and
            I fell in love. After discovering web dev I realized this is What I wanted 
            to do for my career.</p></ScrollAnimation >
         <ScrollAnimation animateIn='slideInRight' duration={0.3}>
-           <p>My skills are currently more geared towards Front End but I plan on picking up back end tools in the future. I will take a course on Back End development with PHP and MySQL this fall so I will have some knowledge by the time I graduate.</p>
+           <p>My skills are currently more geared towards Front End but I plan on picking up back end tools in the future. I will take a course on Back End development with PHP and MySQL this Fall so I will have some knowledge by the time I graduate.</p>
         </ScrollAnimation >
         </div>
     
@@ -260,6 +243,7 @@ class Skills extends React.Component{
         <li><img //onMouseEnter={this.handleHover} 
       //onMouseLeave=  {this.handleLeave}      
              // style={this.state.iconShow} 
+             alt='html icon'
               className='toolIcon' src='https://caleb-ellis.github.io/assets/img/html5.svg'/>
           <p   //style={this.state.iconLabel}
           >HTML5</p>
@@ -268,20 +252,21 @@ class Skills extends React.Component{
               //onMouseEnter={this.handleHover} 
      // onMouseLeave=  {this.handleLeave}      
              // style={this.state.iconShow} 
+             alt='css icon'
               className='toolIcon' src='https://caleb-ellis.github.io/assets/img/css3.svg'/>
           <p //style={this.state.iconLabel}
           >CSS3</p>
         </li>
-         <li><img className='toolIcon' src='https://caleb-ellis.github.io/assets/img/js.png'/>
+         <li><img className='toolIcon' src='https://caleb-ellis.github.io/assets/img/js.png'  alt='JavaScript icon'/>
            <p>JavaScript</p>
            </li>
-         <li><img className='toolIcon' src='https://caleb-ellis.github.io/assets/img/jquery.svg'/>
+         <li><img className='toolIcon' src='https://caleb-ellis.github.io/assets/img/jquery.svg'  alt='jQuery icon'/>
          <p>jQuery</p>
          </li>
-         <li><img className='toolIcon' src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'/>
+         <li><img className='toolIcon' src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'  alt='React.js icon'/>
          <p>React</p>
          </li>
-         <li><img className='toolIcon' src='https://pngimg.com/uploads/mysql/mysql_PNG9.png'/>
+         <li><img className='toolIcon' src='https://pngimg.com/uploads/mysql/mysql_PNG9.png'  alt='MySQL icon'/>
          <p>MySQL</p>
          </li>
               </ul>
@@ -302,7 +287,7 @@ class Experience extends React.Component{
     <div className='experience'>
         <div className='exp'>
         <a href='https://www.agencyq.com/' target='_blank'>
-        <img src='https://res-2.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1397200679/0b7dc65611a21bbd74ccd6c3ae406de6.png'/>
+        <img alt='agencyQ' src='https://res-2.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1397200679/0b7dc65611a21bbd74ccd6c3ae406de6.png'/>
           </a>
           <p className='exp-desc q'>While at agencyQ I learned how to work in an agile environment 
           and was involved in redesigning the company homepage using the Hubspot  CMS, 
@@ -311,7 +296,7 @@ class Experience extends React.Component{
         </div>  
          <div className='exp'>
         <a href='https://midamedical.org/' target='_blank'>
-        <img className='mida' src='https://midamedical.org/static/media/logo.8b7d81c0.png'/>
+        <img alt='MidaMedical' className='mida' src='https://midamedical.org/static/media/logo.8b7d81c0.png'/>
          
           <span className='midamed'> MIDA</span></a>  <p className='exp-desc'> I worked in a remote team communicating via Slack and Github. I helped modify the company website written in React.js, wrote some unit tests, and I helped design an account-based database system for the website.</p>
         </div>  
@@ -333,11 +318,11 @@ class Projects extends React.Component{
   <div className='proj-desc-name'>
   <h3><a href='https://codepen.io/aquat3/full/xmQrXY' target='_blank'>jQuery Calculator</a></h3>
   <hr/>
-  <img 
+  <img  alt='html icon'
              src='http://aux.iconspalace.com/uploads/17575329542047224463.png'/>
-             <img 
+             <img  alt='css icon'
              src='https://www.icustomweb.com/icw-content/uploads/2018/01/css3-white.png'/>
-               <img 
+               <img  alt='jQuery icon'
              src='https://flaticons.net/gd/makefg.php?i=icons/Brand%20Identity/Logo-JQuery.png&r=255&g=255&b=255'/>
   </div>
 </div>
@@ -347,11 +332,11 @@ class Projects extends React.Component{
   <div className='proj-desc-name'>
   <h3><a href='https://codepen.io/aquat3/full/exeVeR' target='_blank'>Tic Tac Toe</a></h3>
   <hr/>
-  <img 
+  <img  alt='html icon'
              src='http://aux.iconspalace.com/uploads/17575329542047224463.png'/>
-             <img 
+             <img  alt='css icon'
              src='https://www.icustomweb.com/icw-content/uploads/2018/01/css3-white.png'/>
-               <img 
+               <img  alt='JavScript icon'
              src='https://i.ibb.co/Y0w9cXj/js.png'/>
   </div>
 </div>
@@ -359,13 +344,13 @@ class Projects extends React.Component{
         <div className='proj quotes'>
         <div className='proj-desc'>
   <div className='proj-desc-name'>
-  <h3><a href='https://codepen.io/aquat3/full/QPMxNL' target='_blank'>Random Quote Generator</a></h3>
+  <h3><a href='https://codepen.io/aquat3/full/QPMxNL' target='_blank'>Random Quotes</a></h3>
   <hr/>
-  <img 
+  <img  alt='html icon'
              src='http://aux.iconspalace.com/uploads/17575329542047224463.png'/>
-             <img 
+             <img  alt='css icon'
              src='https://www.icustomweb.com/icw-content/uploads/2018/01/css3-white.png'/>
-               <img 
+               <img  alt='JavScript icon'
              src='https://i.ibb.co/Y0w9cXj/js.png'/>
   </div>
 </div>
@@ -376,11 +361,11 @@ class Projects extends React.Component{
   <div className='proj-desc-name'>
   <h3><a href='#'>Portfolio Website</a></h3>
   <hr/>
-  <img 
+  <img  alt='html icon'
              src='http://aux.iconspalace.com/uploads/17575329542047224463.png'/>
-             <img 
+             <img  alt='css icon'
              src='https://www.icustomweb.com/icw-content/uploads/2018/01/css3-white.png'/>
-               <img 
+               <img  alt='JavScript icon'
              src='https://png.pngtree.com/svg/20170807/react_1353124.png'/>
   </div>
 </div>
@@ -407,11 +392,13 @@ class Footer extends React.Component{
 </li>
           <li className='cont'>
           <a href="tel:240-704-2290">
-          <img src='https://www.rightnetworks.com/assets/phone-icon-circle-white.png'/>
+          <img  alt='phone icon'
+          src='https://www.rightnetworks.com/assets/phone-icon-circle-white.png'/>
           <a className='phone'>240-704-2290</a>
           </a>
           <a href="mailto:aquat3@gmail.com">
-          <img src='https://unite-production.s3.amazonaws.com/tenants/firstlutheran/pictures/113451/stjem_orig.png'/>
+          <img  alt='email icon'
+          src='https://unite-production.s3.amazonaws.com/tenants/firstlutheran/pictures/113451/stjem_orig.png'/>
           <a className='email'>aquat3@gmail.com</a>
           </a>
           </li>
@@ -424,15 +411,18 @@ class Footer extends React.Component{
            <li className='socialIcons'>
            
           <a href='https://codepen.io/aquat3/' target='_blank'> 
-          <img src='https://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-White-Large.png'/>
+          <img  alt='codepen icon'
+          src='https://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-White-Large.png'/>
            </a>
 
             <a href='https://github.com/aquat3/' target='_blank'>
-            <img src='https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg'/>
+            <img  alt='github icon'
+            src='https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg'/>
               </a>
            
              <a href='https://www.linkedin.com/in/tofik-mahdi-280b5a139/' target='_blank'>
-              <img src='https://www.igbaffiliate.com/sites/all/themes/clarion_theme/images/linkedin.png'/>
+              <img  alt='linkedin icon'
+              src='https://www.igbaffiliate.com/sites/all/themes/clarion_theme/images/linkedin.png'/>
                </a>
            </li>
         </ul>
