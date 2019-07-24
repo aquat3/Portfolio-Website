f229bd9f1ea5ea4ecfc52ec0d6e7cfe06a05e01e
Tofik Mahdi <aquat3@gmail.com>
2019-07-24 19:36:50 -0400

    Updates

---
+++ precache-manifest.c2dfad16deef6e1b774fe98982435ff4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb146916c533c848cb99c82506fbce6c",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "312db98559c0be8eb839",
+    "url": "/Portfolio-Website/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "fb217c3906fa08132065",
+    "url": "/Portfolio-Website/static/css/main.d5c20933.chunk.css"
+  },
+  {
+    "revision": "312db98559c0be8eb839",
+    "url": "/Portfolio-Website/static/js/2.77df4773.chunk.js"
+  },
+  {
+    "revision": "fb217c3906fa08132065",
+    "url": "/Portfolio-Website/static/js/main.f18fe2cf.chunk.js"
+  },
+  {
+    "revision": "b02ecd064c38077ee4dc",
+    "url": "/Portfolio-Website/static/js/runtime~main.e524495c.js"
+  },
+  {
+    "revision": "2a4cddc63cc70fbe561b1fd1a7552374",
+    "url": "/Portfolio-Website/static/media/TicTacToe.2a4cddc6.jpg"
+  },
+  {
+    "revision": "b3e3615c0b93f47f20daa5b3890e141d",
+    "url": "/Portfolio-Website/static/media/calculator.b3e3615c.jpg"
+  },
+  {
+    "revision": "631cbff734720adf634e903ab54024f8",
+    "url": "/Portfolio-Website/static/media/portfoliopic.631cbff7.png"
+  },
+  {
+    "revision": "19d67e7f4ff724c3267268bf385c6357",
+    "url": "/Portfolio-Website/static/media/quotes.19d67e7f.jpg"
+  }
+]);
